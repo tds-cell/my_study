ac2089a5e46e87ced7a005189fc1263020e8dca4
tds-cell <michealjfox0313@yahoo.co.jp>
2021-05-06 21:27:04 +0900

    動画読み込みファイルの移動

--- new_my_app.py
+++ new_my_app.py
@@ -25,7 +25,52 @@ class Model():
         self.frame_count = tk.DoubleVar(value=1.0)
         #スライダーの位置変数
         self.slide_num = tk.DoubleVar(value=1.0)
-    
+
+    #動画ファイルを読み込み
+    def load_movie_file(self):
+        #動画を新たに読み込む場合リセットが必要
+        if self.ext != None:
+            self.scale_bar.pack_forget()
+            self.slide_num.set(1.0)
+            self.img_num = 0
+
+        self.iDir_m = os.path.abspath(
+            os.path.dirname(__file__))        
+        #これで今作業中のディレクトリ(=self.iDir)からのフォルダーダイアログを表示する
+        #さらにfiletypeを全てにしておく
+        self.filePath = filedialog.askopenfilename(
+            filetypes=self.fTyp, initialdir=self.iDir_m
+            )
+
+        #拡張子を取得
+        _, self.ext = os.path.splitext(self.filePath)
+        
+        if self.ext == ".MOV" or ".mp4":
+            #setメソッドでテキストボックスに選択したファイルのパスを入れる
+            self.file_name.set(self.filePath)
+            self.cap = cv2.VideoCapture(self.filePath)
+            #読み込んだ動画の最初の画像でサムネイルを表示
+            self.cv2ImageTk()
+            self.canvas_video.create_image( 
+                0, 0, image=self.img, anchor='nw'
+            )
+            
+            #読み込んだ動画の総フレーム数をカウント
+            self.frame_count = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)
+            self.first_frame = self.frame_count / self.frame_count
+            self.scale_bar_button()
+
+        #キャンセル処理の時のエラー対策
+        elif self.filePath == "":
+            return 
+        #動画ファイルじゃない時のエラー対策
+        else :
+            messagebox.showwarning(
+                "waring",
+                "filetype is not [.mov]"
+            )
+            self.ext = None
+  
     #動画ではあるが画像イメージつなげて
     #動画とするためimageと名付けた
     def create_image(self, path):
@@ -126,7 +171,7 @@ class Control():
         self.model = model
         self.view = view
     
-        self.set_events():
+        self.set_events()
 
     def set_events(self):
         #動画ロード実行
